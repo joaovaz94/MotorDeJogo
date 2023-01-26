df400b80d3bfb0bc8fda06d8ea3986f22d88dbba
joaovaz94 <joaovaz94.4@gmail.com>
2023-01-26 10:13:06 -0300

    Collider com debug

--- src/Colider.cpp
+++ src/Colider.cpp
@@ -3,6 +3,7 @@
 #include "include/Game.h"
 #include <math.h>
 
+
 Collider::Collider(GameObject &associated, Vec2 scale, Vec2 offset) : Component(associated) {
     this->scale = scale;
     this->offset = offset;
@@ -16,7 +17,26 @@ void Collider::Update(float dt) {
 }
 
 void Collider::Render() {
+#ifdef DEBUG
+	Vec2 center( box.Center() );
+	SDL_Point points[5];
 
+	Vec2 point = Vec2::Rotate(Vec2(box.x, box.y) - center, associated.angleDeg /(180/M_PI) ) + center - Camera::pos;
+	points[0] = {(int)point.x, (int)point.y};
+	points[4] = {(int)point.x, (int)point.y};
+	
+	point = Vec2::Rotate(Vec2(box.x + box.w, box.y) - center,  associated.angleDeg/(180/M_PI) )+ center - Camera::pos;
+	points[1] = {(int)point.x, (int)point.y};
+	
+	point = Vec2::Rotate(Vec2(box.x + box.w, box.y + box.h) - center, associated.angleDeg/(180/M_PI) )+ center - Camera::pos;
+	points[2] = {(int)point.x, (int)point.y};
+	
+	point = Vec2::Rotate(Vec2(box.x, box.y + box.h) - center, associated.angleDeg/(180/M_PI) )+ center - Camera::pos;
+	points[3] = {(int)point.x, (int)point.y};
+
+	SDL_SetRenderDrawColor(Game::GetInstance().GetRenderer(), 255, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLines(Game::GetInstance().GetRenderer(), points, 5);
+#endif // DEBUG
 }
 
 bool Collider::Is(std::string type) {
